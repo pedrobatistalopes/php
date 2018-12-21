bb65d27df40ed6d440e5c00fe320ceffdf434279
Pedro Batista Lopes <ped.blopes@gmail.com>
2018-12-21 19:40:18 -0200

    resolvi o conflito de olas

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 <<<<<<< HEAD
 	echo"Olá mundo 456";
     
-    echo"Olá hcode";
-=======
-	echo"Olá mundo 123";
-	echo"Olá Pedro";
->>>>>>> 1ab247446b3376b71f2d8426cb652caa4ae4fcf0
+    echo"Olá hcode! e Olá udemy";
+
 ?>
